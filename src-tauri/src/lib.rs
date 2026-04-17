c66975147cf7882e7d3d5da9b6684526c028e357
Mika Westerlaken-Burge <mikathedev@proton.me>
2026-04-16 21:50:17 -0400

    Add show selection and dynamic options loading functionality

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,48 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use downlowd::Client;
 use serde_json::Value;
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use std::env;
 
+
+#[derive(Deserialize)]
+struct Show {
+    path: String,
+    episode: u32,
+    season: String,
+    show: bool,
+    url: String,
+    #[serde(rename = "episode links")]  // CHANGED: handle the space in key
+    episode_links: HashMap<String, HashMap<String, String>>,
+}
+fn get_json_data() -> String {
+    return std::fs::read_to_string("shows.json").unwrap()
+}
+#[tauri::command]
+fn get_options() -> Vec<String> {
+    let content = get_json_data();
+    let shows: HashMap<String, Show> = serde_json::from_str(&content).unwrap();
+    let mut options: Vec<String> = Vec::new();
+    for (show, _) in shows.iter() {
+        options.push(show.to_string());
+    }
+    options
+}
+
 fn get_link(show: &str) -> String {
-    
+    let content = get_json_data();
+    let shows: HashMap<String, Show> = serde_json::from_str(&content).unwrap();
+    let show_info = &shows[show];
+    let season = &show_info.season;
+    let episode = &show_info.episode.to_string();
+    let episode_link = &show_info.episode_links[season][episode];
+
+    episode_link.to_string()
 }
 #[tauri::command]
 fn download(show: &str) -> String {
-   
+   get_link(show)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -17,6 +51,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![download])
+        .invoke_handler(tauri::generate_handler![get_options])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
   import { convertFileSrc, invoke } from "@tauri-apps/api/core";
+  import {getCurrentWindow} from "@tauri-apps/api/window";
   import { open } from '@tauri-apps/plugin-dialog';
   import VideoPlayer from 'svelte-video-player';
 
 
-  let show = ""
+  let show = $state("Community")
   let src = $state("")
   let dialog: HTMLDialogElement
-
+  let options = $state([])
   let playing = $state(false);
 
-  function darkMode() {
-   document.body.classList.toggle('dark');
+  function closeWindow() {
+     const window = getCurrentWindow();
+     window.close();
+  }
+  async function get_options() {
+   options = await invoke("get_options")
+   console.log(options)
   }
   // CHANGED: svelte action to track video state
   function trackPlaying(node: HTMLElement) {
    const video = node.querySelector('video');
    if (!video) return {};
-   const onPlay = () => {
-    playing = true
-    darkMode()
-   };
-   const onPause = () => {
-    playing = false;
-   }
+   const onPlay = () => { playing = true };
+   const onPause = () => { playing = false; }
    video.addEventListener('play', onPlay);
    video.addEventListener('pause', onPause);
    return {
@@ -34,9 +35,11 @@
    };
   }
 
+  function downloadFile() {
+   invoke("download", { show: show }).then((res) => {console.log(res)})
+  }
   async function getVideoPath(show: string) {
-   //const file: string = await invoke("get_video_path", { show: show })
-   let file = 'F:/Community/0411.mp4'
+   const file: string = await invoke("get_video_path", { show: show })
    src = convertFileSrc(file)
   }
   async function getVideoPath2(show: string) {
@@ -44,14 +47,33 @@
    let file = 'F:/Community/0410.mp4'
    src = convertFileSrc(file)
   }
-  invoke("download", { show: show })
+
   getVideoPath(show)
+  get_options()
 </script>
 
 <nav>
+ <select bind:value={show}>
+    {#each options as show}
+     <option value={show}>{show}</option>
+    {/each}
+
+ </select>
  <button onclick={() => getVideoPath(show)}>Get Video</button>
+ <button onclick={() => downloadFile()}>run download</button>
  <button class="open-modal" onclick={() => dialog.showModal()}>Open Modal</button>
  <div data-tauri-drag-region></div>
+ <button onclick={closeWindow}>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+   <path
+           d="M18 6L6 18M6 6L18 18"
+           stroke="currentColor"
+           stroke-width="2"
+           stroke-linecap="round"
+           stroke-linejoin="round"
+   />
+  </svg>
+ </button>
 </nav>
 <main class="container">
  <div class="video-player" use:trackPlaying>
@@ -111,6 +133,6 @@
   max-width: calc(95vh * (16 / 9));
  }
 
- body.dark { background: black; color: white; }
+
 
 </style>
